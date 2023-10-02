86fa1b243ce3973b5d754560e4e06ae4708f6ef8
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-02 12:21:44 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dennis Mutwiri Mugambi on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
